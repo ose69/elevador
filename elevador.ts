2a44a0fae1bb14a05cf1f6ea6b8a80cb95a2ff17
ose69 <47892767+ose69@users.noreply.github.com>
2019-05-31 22:13:16 -0500

    ts
    avanzando

--- elevador.ts
+++ elevador.ts
@@ -1,3 +1,53 @@
 class Elevador{
     pisosTotales:number;
+    pisoActual:number;
+    estadoPuerta:string;
+    constructor(pisosTotal) {
+        this.estadoPuerta="abierta";
+        this.pisoActual=1;
+        this.pisosTotales=pisosTotal;
+    }
+    public ePuerta(estado){
+        if (this.estadoPuerta!="abierta") {
+            return "puerta cerrada" + estado;
+        }
+        else "abierta";
+    }
+    public abrirPuerta(){
+        if(this.estadoPuerta!="abierta"){
+            return "se abre puerta";
+        }
+    }
+    public cerrarPuerta(){
+        if(this.estadoPuerta==="abierta"){
+            return "cerrando puerta";
+        }
+    }
+    private subir() {
+        if (this.pisoActual===1 &&this.pisoActual<this.pisosTotales) {
+            return "subiendo";
+        }
+    } 
+    private bajar() {
+        if (this.pisoActual>1 && this.pisoActual<=this.pisosTotales) {
+            return "bajando";
+        }
+    }
+    private irA(destino) {
+        if (destino > this.pisosTotales || destino < this.pisosTotales) {
+            return false;
+        }
+        else {
+            if (destino > this.pisoActual) {
+                this.subir(destino);
+            }
+            else if (destino < this.pisoActual) {
+                this.bajar(destino);
+            }
+            else {
+                return 'El destino es el piso actual.';
+            }
+        }
+    }
+
 }
